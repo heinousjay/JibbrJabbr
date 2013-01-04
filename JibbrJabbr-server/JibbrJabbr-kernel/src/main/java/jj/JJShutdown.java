0df11726a2bfb7584fb6f60e8413462acda33127
Jay <github@heinousjay.com>
2013-01-03 22:06:46 -0500

    comments

--- JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/JJShutdown.java
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/JJShutdown.java
@@ -1,5 +1,11 @@
 package jj;
 
+/**
+ * implement to receive a notification of server shutdown
+ * 
+ * @author jason
+ *
+ */
 public interface JJShutdown {
 
 	void stop();
--- JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/JJStartup.java
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/JJStartup.java
@@ -1,5 +1,11 @@
 package jj;
 
+/**
+ * implement to receive a notification of server start up
+ * 
+ * @author jason
+ *
+ */
 public interface JJStartup {
 
 	void start() throws Exception;
