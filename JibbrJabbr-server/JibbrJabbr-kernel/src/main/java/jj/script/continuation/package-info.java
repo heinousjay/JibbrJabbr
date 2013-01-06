6e603435812c60c21fbaa0837ba7986a71d9f919
Jay <github@heinousjay.com>
2013-01-06 10:37:51 -0500

    documentation

--- JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/script/continuation/package-info.java
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/script/continuation/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Simple objects used to trigger continuations
- * in a typesafe manner
+ * in a typesafe manner.  Conceptually, this also
+ * includes the classes in the jj.jqmessage package,
+ * as a JQueryMessage can be used as a continuation.
  */
 package jj.script.continuation;
