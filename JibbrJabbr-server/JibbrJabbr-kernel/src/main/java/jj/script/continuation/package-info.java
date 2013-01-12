625a8abaab5b1bfd01c3457b22144d654e7d141d
Jay <github@heinousjay.com>
2013-01-12 10:57:59 -0500

    removed an empty package

--- JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/script/continuation/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Simple objects used to trigger continuations
- * in a typesafe manner.  Conceptually, this also
- * includes the classes in the jj.jqmessage package,
- * as a JQueryMessage can be used as a continuation.
- */
-package jj.script.continuation;
