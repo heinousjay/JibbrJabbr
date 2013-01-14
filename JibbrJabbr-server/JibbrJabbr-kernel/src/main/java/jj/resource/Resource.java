bc21400c8f81fc73cc8608fb5249559fbcfffc9c
Jay <github@heinousjay.com>
2013-01-13 22:10:07 -0500

    pretty comments all in a row

--- JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/resource/Resource.java
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/resource/Resource.java
@@ -4,23 +4,71 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
 
+import jj.IOThread;
+
+/**
+ * represents some resource loaded from the file system
+ * @author jason
+ *
+ */
 public interface Resource {
 	
+	/**
+	 * The base name of this resource, which is the filename from the base
+	 * path configured on the server, not including the extension in most cases
+	 * @return
+	 */
 	String baseName();
 	
+	/**
+	 * the arguments used to create this resource
+	 * @return
+	 */
 	Object[] creationArgs();
-
+	
+	/**
+	 * the path to this resource in the file system
+	 * @return
+	 */
 	Path path();
 	
+	/**
+	 * indicates if the resource is out of date
+	 * @return
+	 * @throws IOException
+	 */
+	@IOThread
 	boolean needsReplacing() throws IOException;
 	
+	/**
+	 * time this resource was last modified
+	 * @return
+	 */
 	FileTime lastModified();
 
+	/**
+	 * uri to this resource, expressed as a path relative to
+	 * the absolute root that we serve
+	 * @return
+	 */
 	String uri();
 	
+	/**
+	 * absolute uri using our canonical domain
+	 * @return
+	 */
 	String absoluteUri();
 	
+	/**
+	 * sha1 of the file bytes
+	 * @return
+	 */
 	String sha1();
 	
+	/**
+	 * the mime of the resource, including charset if it
+	 * is textual.  (always utf-8)
+	 * @return
+	 */
 	String mime();
 }
