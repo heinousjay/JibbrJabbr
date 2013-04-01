3c41cc8158908ed40c7472d35bfe1adf78fc73ec
Jay <github@heinousjay.com>
2013-03-31 22:40:26 -0400

    fixing up the thread organization annotations

--- JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/IOThread.java
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/IOThread.java
@@ -1,9 +1,18 @@
 package jj;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Marker to document that a method can be called
  * from the io pool
  * @author jason
  *
  */
+@Documented
+@Target({ElementType.METHOD,ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface IOThread {}
--- JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/ScriptThread.java
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/ScriptThread.java
@@ -1,3 +1,16 @@
 package jj;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a method meant to be called from a script thread.
+ * @author jason
+ */
+@Documented
+@Target({ElementType.METHOD,ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ScriptThread {}
