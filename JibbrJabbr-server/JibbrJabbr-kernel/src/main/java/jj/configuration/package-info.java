228e5fc71e1bcf24636156fc632211fbe5887607
Jay <github@heinousjay.com>
2013-06-15 12:47:54 -0400

    documentation

---
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/main/java/jj/configuration/package-info.java
@@ -0,0 +1,25 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Defines the configuration system, which allows for type-safe
+ * access to configuration items that are passed on the command
+ * line, perhaps from a config file?  and in the planning, exposure
+ * to a REPL for runtime changes
+ * 
+ * @author jason
+ *
+ */
+package jj.configuration;
