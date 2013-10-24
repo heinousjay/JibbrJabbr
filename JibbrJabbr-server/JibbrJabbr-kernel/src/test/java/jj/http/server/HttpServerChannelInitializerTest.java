1b3da663f409b0e8f3627316716e68ab08cdec4a
Jay <github@heinousjay.com>
2013-10-23 20:30:59 -0400

    making a test prettier and strengthening the verifications

--- JibbrJabbr-server/JibbrJabbr-kernel/src/test/java/jj/http/server/HttpServerChannelInitializerTest.java
+++ JibbrJabbr-server/JibbrJabbr-kernel/src/test/java/jj/http/server/HttpServerChannelInitializerTest.java
@@ -16,9 +16,6 @@
 package jj.http.server;
 
 import static jj.http.server.HttpServerChannelInitializer.PipelineStages.*;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.sameInstance;
-import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.*;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelPipeline;
@@ -30,11 +27,10 @@ import io.netty.handler.stream.ChunkedWriteHandler;
 
 import javax.inject.Provider;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -45,43 +41,39 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class HttpServerChannelInitializerTest {
 
-	HttpServerChannelInitializer hsci;
+	
 	@Mock Provider<JJEngineHttpHandler> engineProvider;
+	
+	@InjectMocks HttpServerChannelInitializer hsci;
+	
 	@Mock JJEngineHttpHandler engine;
 	@Mock SocketChannel ch;
-	@Mock ChannelPipeline pipeline;
-	@Captor ArgumentCaptor<ChannelHandler> handlerCaptor; 
+	@Mock ChannelPipeline pipeline; 
 	
-	@Before
-	public void before() {
+	@Test
+	public void test() throws Exception {
 		
+		// given
 		given(engineProvider.get()).willReturn(engine);
 		given(ch.pipeline()).willReturn(pipeline);
 		given(pipeline.addLast(anyString(), any(ChannelHandler.class))).willReturn(pipeline);
 		
-		hsci = new HttpServerChannelInitializer(engineProvider);
-	}
-	
-	@Test
-	public void test() throws Exception {
+		
+		// when
 		hsci.initChannel(ch);
 		
-		verify(pipeline).addLast(eq(Decoder.toString()), handlerCaptor.capture());
-		assertTrue(handlerCaptor.getValue() instanceof HttpRequestDecoder);
 		
-		verify(pipeline).addLast(eq(Aggregator.toString()), handlerCaptor.capture());
-		assertTrue(handlerCaptor.getValue() instanceof HttpObjectAggregator);
+		// then
+		InOrder i = inOrder(pipeline);
 		
-		verify(pipeline).addLast(eq(Encoder.toString()), handlerCaptor.capture());
-		assertTrue(handlerCaptor.getValue() instanceof HttpResponseEncoder);
+		i.verify(pipeline).addLast(eq(Decoder.toString()), isA(HttpRequestDecoder.class));
+		i.verify(pipeline).addLast(eq(Aggregator.toString()), isA(HttpObjectAggregator.class));
+		i.verify(pipeline).addLast(eq(Encoder.toString()), isA(HttpResponseEncoder.class));
+		i.verify(pipeline).addLast(eq(ChunkedWriter.toString()), isA(ChunkedWriteHandler.class));
+		i.verify(pipeline).addLast(JJEngine.toString(), engine);
 		
-		verify(pipeline).addLast(eq(ChunkedWriter.toString()), handlerCaptor.capture());
-		assertTrue(handlerCaptor.getValue() instanceof ChunkedWriteHandler);
-		
-		verify(pipeline).addLast(eq(JJEngine.toString()), handlerCaptor.capture());
-		assertThat(engine, is(sameInstance(handlerCaptor.getValue())));
-		
-		// this test acts as an inventory of the handlers
+		// this test acts as an inventory of the handlers, so
+		// ensure nothing else happened
 		verifyNoMoreInteractions(pipeline);
 	}
 
