7e1181c40b1735d81e5564bed04d77182b869118
Jay <github@heinousjay.com>
2014-02-08 18:47:05 -0500

    making a test stricter

--- kernel/src/test/resources/app/helpers.js
+++ kernel/src/test/resources/app/helpers.js
@@ -4,8 +4,22 @@
 var amp = /&/g;
 var quot = /"/g;
 var lt = /</g;
+
 exports.dehtml = function(input) {
-	require('./helpers2').printHelpersId();
+	java.lang.System.err.println("********************************************************");
+	java.lang.System.err.println("********************************************************");
+	java.lang.System.err.println("**********                                    **********");
+	java.lang.System.err.println("********************************************************");
+	java.lang.System.err.println("********************************************************");
+	java.lang.System.err.println("*   hi! from helpers.js dehtml function.  enable me!   *");
+	java.lang.System.err.println("********************************************************");
+	java.lang.System.err.println("********************************************************");
+	java.lang.System.err.println("**********                                    **********");
+	java.lang.System.err.println("********************************************************");
+	java.lang.System.err.println("********************************************************");
+	// for the moment, this  is trouble but
+	// uncomment when this will run!
+	//require('./helpers2').printHelpersId();
 	return input.replace(amp, '&amp;').replace(lt, '&lt;').replace(quot, '&quot;');
 };
 
