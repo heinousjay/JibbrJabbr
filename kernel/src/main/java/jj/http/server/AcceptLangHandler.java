9e26c6c0ecb41f72a9fe768c555aa1316f5f30e4
Jay <github@heinousjay.com>
2014-05-07 22:26:20 -0400

    parsing the accept-lang header

---
+++ kernel/src/main/java/jj/http/server/AcceptLangHandler.java
@@ -0,0 +1,123 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jj.http.server;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import io.netty.handler.codec.http.HttpHeaders;
+
+/**
+ * <p>
+ * Parses the accept-lang header from a given headers set,
+ * and makes the contents available as a {@link List} of 
+ * {@link Locale}s in order of preference as determined by
+ * q-values
+ * 
+ * <p>
+ * The algorithm is kinda wasteful of memory.  oh well
+ * 
+ * @author jason
+ *
+ */
+class AcceptLangHandler {
+	
+	private static final class SortableLocale implements Comparable<SortableLocale> {
+
+		final Locale locale;
+		final BigDecimal qValue;
+		
+		SortableLocale(final Locale locale, final String qValue) {
+			this.locale = locale;
+			this.qValue = new BigDecimal(qValue);
+		}
+		
+		@Override
+		public int compareTo(SortableLocale o) {
+			// sort them in reverse!
+			return o.qValue.compareTo(qValue);
+		}
+		
+	}
+	
+	private static final Pattern LIST_SPLITTER = Pattern.compile("\\s*,\\s*");
+	private static final Pattern VALUE_PARSER = 
+		// this pattern is kinda complicated to read, it is meant to match the grammar specified at
+		// http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
+		// group 1 is the lang
+		// group 2 is the country, if any
+		// group 3 is the q-value, if any
+		Pattern.compile("^(?:([a-zA-Z]{1,8})(?:-([a-zA-Z]{1,8}))?|\\*)(?:;q=((?:0\\.\\d{1,3})|1|0))?$");
+	
+	private final List<Locale> locales;
+	
+	private boolean badRequest = false;
+
+	// need to get a hold of the server default locale somewhere?
+	AcceptLangHandler(final HttpHeaders requestHeaders) {
+		
+		String headerValue = requestHeaders.get(HttpHeaders.Names.ACCEPT_LANGUAGE);
+		
+		locales = parseLocales(LIST_SPLITTER.split(headerValue));
+	}
+	
+	private List<Locale> parseLocales(String[] incomingValues) {
+		ArrayList<SortableLocale> holder = new ArrayList<>(incomingValues.length);
+		
+		for (String value : incomingValues) {
+			Matcher matcher = VALUE_PARSER.matcher(value);
+			if (!matcher.matches()) { // it either matches or we hates you.  HATES
+				badRequest = true;
+				break;
+			}
+			String lang = matcher.group(1);
+			String country = matcher.group(2);
+			String qValue = matcher.group(3);
+			if (qValue == null) qValue = "1";
+			Locale locale = country == null ?
+				(new Locale(lang)) :
+				(new Locale(lang, country));
+			
+			if (!"0".equals(qValue)) {
+				// "0" means don't use... which really we can't guarantee
+				holder.add(new SortableLocale(locale, qValue));
+			}
+		}
+		
+		Collections.sort(holder);
+		
+		ArrayList<Locale> result = new ArrayList<>(holder.size());
+		for (SortableLocale sortableLocale : holder) {
+			result.add(sortableLocale.locale);
+		}
+		
+		return Collections.unmodifiableList(result);
+	}
+
+	public List<Locale> locales() {
+		return locales;
+	}
+	
+	public boolean isBadRequest() {
+		return badRequest;
+	}
+	
+}
---
+++ kernel/src/test/java/jj/http/server/AcceptLangHandlerTest.java
@@ -0,0 +1,87 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jj.http.server;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.Locale;
+
+import io.netty.handler.codec.http.HttpHeaders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author jason
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AcceptLangHandlerTest {
+
+	@Mock HttpHeaders requestHeaders;
+	
+	AcceptLangHandler arh;
+	
+	private void establishObjects(String value) {
+		
+		given(requestHeaders.get(HttpHeaders.Names.ACCEPT_LANGUAGE)).willReturn(value);
+		arh = new AcceptLangHandler(requestHeaders);
+	}
+	
+	@Test
+	public void test1() {
+		establishObjects("da, en-gb;q=0.8, en;q=0.7");
+		assertThat(arh.locales().size(), is(3));
+		assertThat(arh.locales().get(0), is(new Locale("da")));
+		assertThat(arh.locales().get(1), is(new Locale("en", "GB")));
+		assertThat(arh.locales().get(2), is(new Locale("en")));
+	}
+	
+	@Test
+	public void test2() {
+		establishObjects("en-US,en;q=0.8,ar;q=0.6");
+		assertThat(arh.locales().size(), is(3));
+		assertThat(arh.locales().get(0), is(new Locale("en", "US")));
+		assertThat(arh.locales().get(1), is(new Locale("en")));
+		assertThat(arh.locales().get(2), is(new Locale("ar")));
+	}
+	
+	@Test
+	public void test3() {
+		establishObjects("en-US;q=0.4,en;q=0.7,ar;q=0.9");
+		assertThat(arh.locales().size(), is(3));
+		assertThat(arh.locales().get(0), is(new Locale("ar")));
+		assertThat(arh.locales().get(1), is(new Locale("en")));
+		assertThat(arh.locales().get(2), is(new Locale("en", "US")));
+	}
+	
+	@Test
+	public void testBadRequests() {
+		establishObjects("this is not a value accept-lang string");
+		assertThat(arh.isBadRequest(), is(true));
+		
+		establishObjects("da-da-da-da;q=2");
+		assertThat(arh.isBadRequest(), is(true));
+		
+		establishObjects("da;q=2");
+		assertThat(arh.isBadRequest(), is(true));
+	}
+
+}
