0e4ebd554378ce326f8b367b8e160e525ef18357
Jay <github@heinousjay.com>
2014-06-08 15:53:19 -0400

    need a place to collect the config values
    the idea is that we really just have a map that represents the values
    that get set by the configuration script, and the entire set of
    configuration values gets updated atomically for the sake of the rest of
    the system.
    
    code-gen to read things out might get weird.  not sure yet, javassist
    sometimes gets caught up on generics

---
+++ kernel/src/main/java/jj/configuration/ConfigurationCollector.java
@@ -0,0 +1,85 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jj.configuration;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import jj.conversion.Converters;
+
+/**
+ * server component used to implement the configuration API. it's
+ * really just a set of hash maps and a facade to the conversion
+ * system
+ * 
+ * @author jason
+ *
+ */
+@Singleton
+public class ConfigurationCollector {
+	
+	private final AtomicReference<Map<String, Object>> current = new AtomicReference<>();
+	private HashMap<String, Object> inProgress = new HashMap<>();
+	private final Converters converters;
+	
+	@Inject
+	ConfigurationCollector(final Converters converters) {
+		this.converters = converters;
+	}
+	
+	/**
+	 * This is the interface for the API modules
+	 * @param key
+	 * @param value
+	 */
+	public void addConfigurationElement(String key, Object value) {
+		inProgress.put(key, value);
+	}
+	
+	public void addConfigurationMultiElement(String key, Object value) {
+		if (!inProgress.containsKey(key)) {
+			inProgress.put(key, new ArrayList<Object>());
+		}
+	}
+	
+	<T> T get(String key, Class<T> type) {
+		Map<String, Object> map = current.get();
+		assert map != null : "configuration is not yet complete, cannot read!";
+		return converters.convert(map.get(key), type);
+	}
+	
+	void configurationComplete() {
+		
+		for (String key : inProgress.keySet()) {
+			if (inProgress.get(key) instanceof Collection) {
+				@SuppressWarnings({ "unchecked", "rawtypes" })
+				Collection collection =
+					Collections.unmodifiableCollection((Collection)inProgress.get(key));
+				inProgress.put(key, collection);
+			}
+		}
+		
+		current.set(Collections.unmodifiableMap(inProgress));
+		inProgress = new HashMap<>();
+	}
+}
---
+++ kernel/src/test/java/jj/configuration/ConfigurationCollectorTest.java
@@ -0,0 +1,54 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jj.configuration;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import jj.conversion.ConverterSetMaker;
+import jj.conversion.Converters;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author jason
+ *
+ */
+public class ConfigurationCollectorTest {
+	
+	Converters converters;
+	ConfigurationCollector collector;
+
+	@Before
+	public void before() {
+		converters = new Converters(ConverterSetMaker.converters());
+		collector = new ConfigurationCollector(converters);
+	}
+	
+	@Test
+	public void test() {
+		
+		collector.addConfigurationElement("key1", "value1");
+		collector.addConfigurationElement("key2", 2);
+		collector.addConfigurationMultiElement("key3", true);
+		collector.addConfigurationMultiElement("key3", false);
+		
+		collector.configurationComplete();
+		
+		assertThat(collector.get("key1", String.class), is("value1"));
+	}
+
+}
