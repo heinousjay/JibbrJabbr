e26283439a6ee1b6818f5f08f0634238c0dc9cfc
Jay <github@heinousjay.com>
2014-06-10 23:28:50 -0400

    a provider for config objects
    i'm not sure if this implementation will survive, but it's definitely a
    simple way to handle things. register the config object binding, and
    inject it.  voila.

---
+++ kernel/src/main/java/jj/configuration/ConfigurationObjectBinder.java
@@ -0,0 +1,55 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jj.configuration;
+
+import javax.inject.Inject;
+
+import com.google.inject.Binder;
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+
+/**
+ * does a little funky dance so as to provide
+ * injectable configuration objects
+ * 
+ * @author jason
+ *
+ */
+public class ConfigurationObjectBinder {
+	
+	private final Binder binder;
+	
+	public ConfigurationObjectBinder(final Binder binder) {
+		this.binder = binder;
+	}
+
+	public <T> void to(final Class<T> configurationInterface) {
+		
+		Provider<T> provider = new Provider<T>() {
+			
+			@Inject ConfigurationObjectImplementation configurationObjectImplementation;
+			@Inject Injector injector;
+			@Override
+			public T get() {
+				return injector.getInstance(configurationObjectImplementation.implementationClassFor(configurationInterface));
+			}
+		};
+		
+		binder.requestInjection(provider);
+		
+		binder.bind(configurationInterface).toProvider(provider);
+	}
+}
---
+++ kernel/src/main/java/jj/configuration/ConfigurationObjectImplementation.java
@@ -0,0 +1,59 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jj.configuration;
+
+import io.netty.util.internal.chmv8.ConcurrentHashMapV8;
+import io.netty.util.internal.chmv8.ConcurrentHashMapV8.Fun;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Fronts the {@link ConfigurationClassLoader} to cache the results
+ * 
+ * @author jason
+ *
+ */
+@Singleton
+class ConfigurationObjectImplementation {
+	
+	/** mapped interface to implementation */
+	private final ConcurrentHashMapV8<Class<?>, Class<?>> implementations = new ConcurrentHashMapV8<>();
+	
+	private final ConfigurationClassLoader classLoader;
+	
+	@Inject
+	ConfigurationObjectImplementation(final ConfigurationClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
+
+	@SuppressWarnings("unchecked")
+	<T> Class<? extends T> implementationClassFor(final Class<T> configurationInterface) {
+		
+		return (Class<? extends T>)implementations.computeIfAbsent(configurationInterface, new Fun<Class<?>, Class<?>>() {
+
+			@Override
+			public Class<?> apply(Class<?> configurationInterface) {
+				try {
+					return classLoader.makeConfigurationClassFor(configurationInterface);
+				} catch (Exception e) {
+					throw new AssertionError("couldn't implement " + configurationInterface, e);
+				}
+			}
+		});
+	}
+	
+}
