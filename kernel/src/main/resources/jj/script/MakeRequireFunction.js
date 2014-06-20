7311b642a806628ee6733dbc01ae6851dda1f85d
Jay <github@heinousjay.com>
2014-06-19 22:41:22 -0400

    moving a thing

--- kernel/src/main/resources/jj/script/MakeRequireFunction.js
+++ kernel/src/main/resources/jj/script/MakeRequireFunction.js
@@ -1,12 +1,11 @@
-global['//makeRequire'] = function(module) {
+(function(module) {
 	return function(id) {
 		if (!id || typeof id != 'string') {
 			throw new TypeError('argument to require must be a valid module identifier');
 		}
-		var result = global['//require'](id, module.id);
+		var result = module['//requireInner'](id, module.id);
 		if (result['getCause']) {
-			throw new Error(result.getMessage());
+			throw result;
 		}
 		return result;
-	}
-}
+}})(module);
