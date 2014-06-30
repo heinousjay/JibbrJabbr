51e3b1cb717dc5582b15481c338123d192967871
Jay <github@heinousjay.com>
2014-06-29 22:45:54 -0400

    processing css urls in a component
    in the old way, this runs as a post-processing step on a CssResource,
    which is terrible.  truly terrible.
    
    this is interesting to test, there is some circularity, but it's
    working.

---
+++ kernel/src/main/java/jj/css/CssReferenceVersionProcessor.java
@@ -0,0 +1,119 @@
+/*
+ *    Copyright 2012 Jason Miller
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jj.css;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import jj.configuration.resolution.AppLocation;
+import jj.configuration.resolution.Application;
+import jj.http.server.uri.URIMatch;
+import jj.resource.Resource;
+import jj.resource.ResourceFinder;
+import jj.resource.stat.ic.StaticResource;
+
+/**
+ * <p>
+ * processes a string in css format to replace internal URL references
+ * with versioned URLs
+ * 
+ * @author jason
+ *
+ */
+@Singleton
+class CssReferenceVersionProcessor {
+
+	private static final Pattern IMPORT = Pattern.compile("@import\\s+(['\"])(.+?)\\1");
+	private static final Pattern URL = Pattern.compile("url\\((['\"])?(.+?)\\1?\\)");
+	private static final Pattern ABSOLUTE = Pattern.compile("^(?:https?:)?//");
+
+	private final Application app;
+	private final ResourceFinder resourceFinder;
+	
+	@Inject
+	CssReferenceVersionProcessor(
+		final Application app,
+		final ResourceFinder resourceFinder
+	) {
+		this.app = app;
+		this.resourceFinder = resourceFinder;
+	}
+
+	
+	String fixUris(final String css, final StylesheetResource resource) {
+		
+		return fixUrls(fixImports(css, resource), resource);
+	}
+	
+	private String fixUrls(final String css, final StylesheetResource resource) {
+		return doReplacement(css, resource, URL, "url($1", "$1)", StaticResource.class);
+	}
+	
+	private String fixImports(final String css, final StylesheetResource resource) {
+		return doReplacement(css, resource, IMPORT, "@import $1", "$1", StylesheetResource.class);
+	}
+	
+	private String doReplacement(
+		final String css,
+		final StylesheetResource resource,
+		final Pattern pattern,
+		final String prefix,
+		final String suffix,
+		final Class<? extends Resource> type
+	) {
+		// yuck.  the API was never updated
+		StringBuffer sb = new StringBuffer();
+		
+		Matcher matcher = pattern.matcher(css);
+		while (matcher.find()) {
+			String replacement = matcher.group(2);
+			if (!ABSOLUTE.matcher(replacement).find()) {
+				
+				String name;
+				if (replacement.startsWith("/")) {
+					name = replacement.substring(1);
+				} else {
+					name = app.path()
+						.relativize(resource.path().resolveSibling(replacement))
+						.normalize()
+						.toString();
+				
+				}
+				Resource dependency = resourceFinder.loadResource(type, AppLocation.Base, name);
+				
+				if (dependency != null) {
+					dependency.addDependent(resource);
+					URIMatch uriMatch = new URIMatch("/" + name);
+					if (!uriMatch.versioned) {
+						// we only want to replace uris that weren't already versioned
+						replacement = dependency.uri();
+					} else {
+						// replace with the absolute path
+						replacement = "/" + name;
+					}
+				}
+				
+				matcher.appendReplacement(sb, prefix + replacement + suffix);
+			}
+		}
+		matcher.appendTail(sb);
+		
+		return sb.toString();
+	}
+}
