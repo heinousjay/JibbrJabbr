4425d67bbb3b32abc58e0387c6ed01047346809f
Jay <github@heinousjay.com>
2014-07-06 11:35:56 -0400

    repl system improvements
    just adding some helpful functions.  really need to get into character mode and pull in jline and jansi.  or something

--- kernel/src/main/resources/jj/repl/assets/base-repl-system.js
+++ kernel/src/main/resources/jj/repl/assets/base-repl-system.js
@@ -5,6 +5,9 @@ var $$print = (function(){
 			context.current().writeAndFlush(java.lang.String.valueOf(arg()));
 		} catch (e) {
 			context.current().writeAndFlush(java.lang.String.valueOf(e));
+			if ('stack' in e) {
+				context.current().writeAndFlush('\n' + e.stack);
+			}
 		} finally {
 			context.current().writeAndFlush('\n>');
 		}
@@ -17,3 +20,7 @@ var quit = (function() {
 		context.current().close();
 	}
 })();
+
+var objectKeys = function(object) {
+	return JSON.stringify(Object.keys(object));
+}
