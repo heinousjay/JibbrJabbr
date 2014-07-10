598cf4eff46efb04249731c89393998b4d57c3e5
Jay <github@heinousjay.com>
2014-07-10 14:51:26 -0400

    documentation

--- kernel/src/main/java/jj/resource/ResourceKey.java
+++ kernel/src/main/java/jj/resource/ResourceKey.java
@@ -18,6 +18,21 @@ package jj.resource;
 import java.net.URI;
 
 /**
+ * <p>
+ * Identifies a {@link Resource} in the {@link ResourceCache}.
+ * 
+ * <p>
+ * Should always be created via the specific {@link ResourceCreator}
+ * for the given {@link Resource} type. 
+ * 
+ * <p>
+ * Do not rely on implementation details of this class. it is public
+ * to facilitate efficient resource integrations, however, it is
+ * intentionally opaque to support making changes in the resource system.
+ * Details can (and probably will) change during any release. In
+ * particular, do not rely on the string representation containing
+ * any particular text
+ * 
  * @author jason
  *
  */
@@ -26,7 +41,7 @@ public class ResourceKey {
 	private final String toString;
 	private final int hashCode;
 
-	public ResourceKey(final Class<? extends Resource> type, final URI uri) {
+	ResourceKey(final Class<? extends Resource> type, final URI uri) {
 		this.toString = type.getSimpleName().toString() + " at " + uri.toString();
 		this.hashCode = toString.hashCode();
 	}
