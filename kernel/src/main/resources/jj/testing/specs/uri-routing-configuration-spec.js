812349a0b61106464159d82e36c8f5ba64b27bf4
Jay <github@heinousjay.com>
2014-07-13 14:37:28 -0400

    testing some more
    need to understand specifically how to test the output of makeSetter. also need to understand more about the destination
    
    it's funny to spend time on this particular spec since the routes around even used yet.  but that'll change... soon

--- kernel/src/main/resources/jj/testing/specs/uri-routing-configuration-spec.js
+++ kernel/src/main/resources/jj/testing/specs/uri-routing-configuration-spec.js
@@ -8,16 +8,54 @@ var inject = function() {
 	};
 }
 var valueOf = java.lang.String.valueOf;
-
+var bases = [];
+var names = [];
+var stringProperty = function() {}
 var mocks = {
 	'configuration-support' : {
-		makeStringProperty:function() {}
+		makeStringProperty:function(base, name) {
+			bases.push(base);
+			names.push(name);
+			return stringProperty;
+		}
 	}
 };
 
 var require = function(id) {
 	return mocks[id];
 }
+
+describe("exports", function() {
+	
+	describe("welcomeFile", function() {
+		
+		it("sets up using makeStringProperty", function() {
+			expect(bases[0]).toBe(base);
+			expect(names[0]).toBe("welcomeFile");
+		});
+		
+		it("delegates to stringProperty", function() {
+			
+			expect(module.exports.welcomeFile).toBe(stringProperty);
+		});
+	});
+	
+	describe("route", function() {
+		
+		it("contains routing methods", function() {
+			var r = module.exports.route;
+			expect(r.get).toBeDefined();
+			expect(r.GET).toBeDefined();
+			expect(r.post).toBeDefined();
+			expect(r.POST).toBeDefined();
+			expect(r.put).toBeDefined();
+			expect(r.PUT).toBeDefined();
+			expect(r.del).toBeDefined();
+			expect(r.DELETE).toBeDefined();
+		});
+	});
+	
+});
 
 describe("makeSetter", function() {
 	
