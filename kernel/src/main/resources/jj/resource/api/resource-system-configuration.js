6acb4ed7f88a8648642e436fea6911bf3ffe2b88
Jay <github@heinousjay.com>
2014-08-14 12:18:38 -0400

    allow configurations of file watching via config.js

--- kernel/src/main/resources/jj/resource/api/resource-system-configuration.js
+++ kernel/src/main/resources/jj/resource/api/resource-system-configuration.js
@@ -3,5 +3,6 @@ var base = 'jj.resource.ResourceConfiguration.';
 
 module.exports = {
 	ioThreads: support.makeIntProperty(base, 'ioThreads'),
-	maxFileSizeToLoad: support.makeLongProperty(base, 'maxFileSizeToLoad')
+	maxFileSizeToLoad: support.makeLongProperty(base, 'maxFileSizeToLoad'),
+	watchFiles: support.makeBooleanProperty('base', 'watchFiles')
 }
