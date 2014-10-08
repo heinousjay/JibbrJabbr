56a206663da09c96cf9fe5a6c16669a793d408bb
Jay <github@heinousjay.com>
2014-10-08 00:01:21 -0400

    simplifying the Sha1Resource

--- kernel/src/main/java/jj/resource/Sha1Resource.java
+++ kernel/src/main/java/jj/resource/Sha1Resource.java
@@ -16,8 +16,6 @@
 package jj.resource;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -40,6 +38,9 @@ class Sha1Resource extends AbstractResource {
 	
 	// empirical magic numbers - the sha1 hash is 40 hex digits, and the max long value as a decimal is 19 digits
 	private static final Pattern FORMAT = Pattern.compile("^([a-f\\d]{40})(\\d{1,19})$");
+
+	// 59 is the maximum size of the contents as described above.
+	private static final int MAX_SIZE = 59;
 	
 	private final String representedSha;
 	private final long representedFileSize;
@@ -58,29 +59,21 @@ class Sha1Resource extends AbstractResource {
 		// either there is no file at path, so we read in our target bytes to make one
 		// or there is a file, but it's out of date, so we read in our target bytes and make a new one
 		// or it's all good and we use it
-		
-		// 59 is the maximum size of the contents as described above.
-		ByteBuf byteBuffer = Unpooled.buffer(59, 59);
+
 		String sha = null;
 		long size = -1;
 		
 		if (Files.exists(path)) {
-			byteBuffer.writeBytes(Files.readAllBytes(path));
-			Matcher matcher = FORMAT.matcher(byteBuffer.toString(US_ASCII));
-			if (!matcher.matches()) {
-				throw new AssertionError("someone messed with the contents of Sha1Resource file");
+			byte[] bytes = Files.readAllBytes(path);
+			if (bytes.length <= MAX_SIZE) {
+				Matcher matcher = FORMAT.matcher(new String(bytes, US_ASCII));
+				if (matcher.matches()) {
+					sha = matcher.group(1);
+					size = Long.parseLong(matcher.group(2));
+				}
 			}
-			sha = matcher.group(1);
-			size = Long.parseLong(matcher.group(2));
-		} else {
-			sha = SHA1Helper.keyFor(target.resource.path());
-			size = target.resource.size();
-			
-			Files.write(path, (sha + size).getBytes(US_ASCII));
 		}
-		
-		// yuckerdo! but java makes this hard to extract
-		// TODO - make this nicer. you have a test to validate it and everything
+	
 		if (size != target.resource.size()) {
 			sha = SHA1Helper.keyFor(target.resource.path());
 			size = target.resource.size();
