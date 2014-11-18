77872dc8334d2313409ae0b9f4e718e4b253655e
Jay <github@heinousjay.com>
2014-11-18 13:47:15 -0800

    little bit of repl refactoring

--- kernel/src/main/resources/jj/repl/assets/base-repl-system.js
+++ kernel/src/main/resources/jj/repl/assets/base-repl-system.js
@@ -2,22 +2,23 @@
 let [$$print, quit, objectKeys] = (function() {
 	var context = inject('jj.repl.CurrentReplChannelHandlerContext');
 	var showPrompt = true;
+	function prompt() {
+		showPrompt && context.current().writeAndFlush('\n>');
+	}
 	return [
 		function $$print(arg) {
 			try {
 				context.current().writeAndFlush(java.lang.String.valueOf(arg()));
-				// this is intentionally not in a finally because the continuation
-				// causes finally blocks to execute.  whoops
-				showPrompt && context.current().writeAndFlush('\n>');
 			} catch (e) {
 				context.current().writeAndFlush(java.lang.String.valueOf(e));
 				if ('stack' in e) {
 					context.current().writeAndFlush('\n' + e.stack);
 				}
-				// this is intentionally not in a finally because the continuation
-				// causes finally blocks to execute even though it's too early
-				showPrompt && context.current().writeAndFlush('\n>');
 			}
+			// this is intentionally not in a finally because any continuation
+			// causes finally blocks to execute.  whoops. apparently finally is
+			// implemented using java finally.  shouldn't matter, though
+			prompt();
 			showPrompt = true;
 		},
 		function quit() {
