0f51bacae672c507a2d4a0282e8da813c1c8fe27
Jason Miller <jay@heinousjay.com>
2015-05-27 10:59:30 -0700

    doc update

--- kernel/src/main/java/jj/server/package-info.java
+++ kernel/src/main/java/jj/server/package-info.java
@@ -1,11 +1,14 @@
 /**
  * <p>
- * The core loader of the server onto which modules
- * attach.  understands a special kernel module
- * made of classes in this project
+ * Provides a runtime view of the running server, and provides
+ * a registration point to handle internal assets and expose API
+ * pieces.  Also provides the attach point for modules, and exposes
+ * runtime information about the modules that are known to the
+ * system.
  * 
  * <p>
- * 
+ * The main goal is to refactor the kernel into a core and a set
+ * of modules that provide higher level functionality.
  * 
  * @author jason
  *
